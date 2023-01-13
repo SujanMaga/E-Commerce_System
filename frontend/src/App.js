0b0dc29d67a874e19eb94b22888f09f739720fab
Sujan Magar <sujan.magar98765@gmail.com>
2023-01-13 20:25:38 +0545

    React Router V6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
 import Login from "./pages/Login";
-// import Home from "./pages/Home";
-// import Register from "./pages/Register";
+import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Login />;
+  const user = false;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        {/* <Route path="/register" element={<Register />}></Route> */}
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" /> : <Login />}
+        ></Route>
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        ></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
